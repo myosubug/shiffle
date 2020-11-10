3af66bbcd2d4cded19b7a09f29cffe1174abd4b4
sean <seanoh.dev@gamil.com>
2020-11-09 20:47:40 -0700

    working version up to  9TH NOV

---
+++ shiffle/Model/Schedule.swift
@@ -0,0 +1,20 @@
+//
+//  Schedule.swift
+//  shiffle
+//
+//  Created by myosubug on 2020-11-09.
+//
+
+import Foundation
+
+/*
+ date
+ time
+ worker
+ */
+
+
+struct Schedule {
+    var time: Date
+    var name: String
+}
--- shiffle/Views/MainScheduleViewController.swift
+++ shiffle/Views/MainScheduleViewController.swift
@@ -7,59 +7,47 @@
 
 import UIKit
 import FirebaseDatabase
+import FSCalendar
 
 
 
-class MainScheduleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class MainScheduleViewController: UIViewController, FSCalendarDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var tableView: UITableView!
-    var ref: DatabaseReference?
-    var count = 0
     
-    var weekday = ["Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    
+    @IBOutlet var calendar: FSCalendar!
+    @IBOutlet var table: UITableView!
+    
+    let data = ["first", "second", "third"]
+    //var ref: DatabaseReference?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        tableView.delegate = self
-        tableView.dataSource = self
+        calendar.delegate = self
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.dataSource = self
         
-        ref = Database.database().reference()
-        
-        ref?.child("Schedules").observe(.childAdded, with: { (snapshot) in
-            self.count = self.count % 7
-            var temp = self.weekday[self.count]
-            let new = snapshot.value as? String
-            if let updated = new {
-                self.weekday[self.count] = temp + ": " + new! ?? "<no update>"
-                self.tableView.reloadData()
-                self.count = self.count + 1
-            }
-            
-            
-            
-        })
+    }
+    
+    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let format = DateFormatter()
+        format.dateFormat = "EEEE MM-dd-YYYY"
+        let dstring = format.string(from: date)
+        print("\(dstring)")
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return weekday.count
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "WeekdayCell")
-        cell?.textLabel?.text = weekday[indexPath.row]
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
     
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
