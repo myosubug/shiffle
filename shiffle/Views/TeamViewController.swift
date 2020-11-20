503eb84d09a003c137a5281bb104fdd93bb74f79
Cathy <minhhang.chu@ucalgary.ca>
2020-11-20 01:24:20 -0700

    add update and delete team members

--- shiffle/Views/TeamViewController.swift
+++ shiffle/Views/TeamViewController.swift
@@ -62,6 +62,41 @@ class TeamViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let employee = employeesList[indexPath.row]
+        
+        let alertController = UIAlertController(title: employee.name, message: "Do you want to make changes?", preferredStyle: .alert)
+        let updateAction = UIAlertAction(title: "Update", style:.default){ (_) in
+            let id = employee.id
+            
+            let name = alertController.textFields?[0].text
+            let contact = alertController.textFields?[1].text
+            
+            self.updateEmployee(id: id!, name: name!, contact: contact!)
+        }
+        let deleteAction = UIAlertAction(title: "Delete", style:.default){ (_) in
+            self.deleteEmployee(id: employee.id!)
+        }
+        alertController.addTextField { (textField) in
+            textField.text = employee.name
+        }
+        alertController.addTextField { (textField) in
+            textField.text = employee.contact
+        }
+        alertController.addAction(updateAction)
+        alertController.addAction(deleteAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func updateEmployee(id: String, name: String, contact: String){
+        let employee = [ "id": id, "employeeName": name, "employeeContact":contact]
+        refEmployees?.child(id).setValue(employee)
+    }
+    
+    func deleteEmployee(id:String){
+        refEmployees?.child(id).setValue(nil)
+    }
 
     /*
     // MARK: - Navigation
